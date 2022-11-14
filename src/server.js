0c4a0f18f6a95bd60ca0519d5589fb22436de6ea
Duy Phan <duy.910.2015@gmail.com>
2022-11-14 22:31:06 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -9,14 +9,15 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
+  cors({
+    origin: "*",
+    credentials: true,
+  })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
-console.log(p.length);
 trainNlp();
-app.listen(port, () => console.log(`Express server is listening on port ${port}`));
+app.listen(port, () =>
+  console.log(`Express server is listening on port ${port}`)
+);
